1a5769cdcd41a7c478b914bfddd673ff3cf424f0
RUDRA PRATAP SINGH <88509277+rudrasingh001@users.noreply.github.com>
2023-01-20 11:45:54 +0530

    Create Welcome.js

---
+++ Welcome.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Welcome =({name,regNo}) =>  {
+    return(
+
+        <div>
+            <h2>   Welcome To My College</h2>
+            <h1>   Hey,{name}!</h1>
+            <h2>   Registration Number: {regNo}</h2>
+            </div>
+    )
+}
+
+export default Welcome;
